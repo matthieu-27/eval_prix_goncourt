c3e11be2dfc1db5cc94aaf879e071e1b92fe2df8
matthieu-27 <matthieu27@pm.me>
2025-12-09 14:07:18 +0100

    Script create table et insert data

--- BDD/create_table.sql
+++ BDD/create_table.sql
@@ -1,83 +1,54 @@
+CREATE TABLE Book(
+   isbn BIGINT,
+   title VARCHAR(50)  NOT NULL,
+   summary TEXT NOT NULL,
+   main_characters VARCHAR(200)  NOT NULL,
+   release_date DATE NOT NULL,
+   page_number INT NOT NULL,
+   author_name VARCHAR(50)  NOT NULL,
+   author_biography TEXT,
+   PRIMARY KEY(isbn)
+);
+
 CREATE TABLE Selection(
    id INT AUTO_INCREMENT,
    selection_number INT,
    selection_date DATE NOT NULL,
-   number_of_books VARCHAR(50) ,
    PRIMARY KEY(id)
-);
-
-CREATE TABLE Jury(
-   goncourt_year INT,
-   PRIMARY KEY(goncourt_year)
 );
 
 CREATE TABLE Personality(
    id INT AUTO_INCREMENT,
    name VARCHAR(50)  NOT NULL,
    is_president BOOLEAN NOT NULL,
-   goncourt_year INT NOT NULL,
-   PRIMARY KEY(id),
-   FOREIGN KEY(goncourt_year) REFERENCES Jury(goncourt_year)
+   PRIMARY KEY(id)
 );
 
 CREATE TABLE Editor(
    id INT AUTO_INCREMENT,
    editor_name VARCHAR(50)  NOT NULL,
-   editor_price DECIMAL(19,4) NOT NULL,
    PRIMARY KEY(id)
 );
 
-CREATE TABLE Author(
-   id INT AUTO_INCREMENT,
-   author_name VARCHAR(50)  NOT NULL,
-   author_biography TEXT,
-   PRIMARY KEY(id)
-);
-
-CREATE TABLE Awardee(
-   book_isbn CHAR(13) ,
-   number_of_votes INT,
-   PRIMARY KEY(book_isbn)
-);
-
-CREATE TABLE Book(
-   isbn INT,
-   title VARCHAR(50)  NOT NULL,
-   summary TEXT NOT NULL,
-   main_characters VARCHAR(200)  NOT NULL,
-   release_date DATE NOT NULL,
-   page_number INT NOT NULL,
-   author_id INT NOT NULL,
-   PRIMARY KEY(isbn),
-   FOREIGN KEY(author_id) REFERENCES Author(id)
-);
-
-CREATE TABLE Selection_Content(
-   book_isbn INT,
+CREATE TABLE Selection_Books(
+   book_isbn BIGINT,
    selection_id INT,
+   number_of_votes INT,
    PRIMARY KEY(book_isbn, selection_id),
    FOREIGN KEY(book_isbn) REFERENCES Book(isbn),
    FOREIGN KEY(selection_id) REFERENCES Selection(id)
 );
 
 CREATE TABLE Editor_Books(
-   book_isbn INT,
+   book_isbn BIGINT,
    editor_id INT,
+   editor_price DECIMAL(19,4) NOT NULL,
    PRIMARY KEY(book_isbn, editor_id),
    FOREIGN KEY(book_isbn) REFERENCES Book(isbn),
    FOREIGN KEY(editor_id) REFERENCES Editor(id)
 );
 
-CREATE TABLE Awardee_Selection(
-   selection_id INT,
-   book_isbn CHAR(13) ,
-   vote_round INT,
-   PRIMARY KEY(selection_id, book_isbn),
-   FOREIGN KEY(selection_id) REFERENCES Selection(id),
-   FOREIGN KEY(book_isbn) REFERENCES Awardee(book_isbn)
-);
-
-CREATE TABLE Personality_Selection(
+CREATE TABLE Jury(
    selection_id INT,
    personality_id INT,
    PRIMARY KEY(selection_id, personality_id),
